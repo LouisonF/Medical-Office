35f69704e46ad5618fd1d973782678537ba8717c
courtin <francois.courtin1@etu.univ-nantes.fr>
2018-11-22 18:08:36 +0100

    maj DataBase h and cpp

--- DataBase.cpp
+++ DataBase.cpp
@@ -12,8 +12,7 @@
 using namespace std;
 
 DataBase::DataBase() {
-	// TODO Auto-generated constructor stub
-
+	connect_database();
 }
 
 DataBase::~DataBase() {
@@ -30,8 +29,42 @@ void DataBase::connect_database(){
 	   if( rc ) {
 		   cout << "Can't open database: " << endl;
 	   } else {
-	      cout << "Opened database successfully" << endl;
+		   cout << "Opened database successfully" << endl;
 	   }
-	   sqlite3_close(db);
+	   //sqlite3_close(db);
 }
 
+int DataBase::affichage_sql(void *NotUsed, int argc, char **argv, char **azColName) {
+   int i;
+   for(i = 0; i<argc; i++) {
+      cout << azColName[i] << " : " << argv[i] << endl << argv[i] << "vide" << endl;
+   }
+   cout << endl;
+   return 0;
+}
+
+void DataBase::set_database(){
+	string sql;
+	int rc;
+	char *zErrMsg;
+
+	sql = "CREATE TABLE PATIENT("
+			"num_secu TEXT PRIMARY KEY     NOT NULL,"
+			"nom           	TEXT    NOT NULL,"
+			"prenom        	TEXT    NOT NULL,"
+			"date_naissance   TEXT    NOT NULL,"
+			"tel          	TEXT    NOT NULL,"
+			"adresse      	CHAR(50),"
+			"medecin      	TEXT, "
+			"grp_sanguin      TEXT );";
+
+	/* Execute SQL statement */
+	rc = sqlite3_exec(db, sql.c_str(), affichage_sql, 0, &zErrMsg);
+
+	if( rc != SQLITE_OK ){
+	      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	      sqlite3_free(zErrMsg);
+	} else {
+		cout << "Table created successfully" << endl;
+	}
+}
--- DataBase.h
+++ DataBase.h
@@ -15,10 +15,21 @@
 using namespace std;
 
 class DataBase {
+private:
+	sqlite3 *db;
 public:
 	DataBase();
 	virtual ~DataBase();
+	sqlite3 get_db();
+	static int affichage_sql(void*, int, char**, char**);
 	void connect_database();
+	void set_database();
+	typedef int (*sqlite3_afficher_sql)(
+	   void*,    /* Data provided in the 4th argument of sqlite3_exec() */
+	   int,      /* The number of columns in row */
+	   char**,   /* An array of strings representing fields in the row */
+	   char**    /* An array of strings representing column names */
+	);
 };
 
 #endif /* DATABASE_H_ */
